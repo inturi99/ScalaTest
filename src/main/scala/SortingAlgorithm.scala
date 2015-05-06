eaba62b7ae9c6aa6b9f842b8ef8447d846dc05c1
abelia <inturi99@gmail.com>
2015-05-06 13:12:10 +0530

    Implement InsertionSort and Spec

--- src/main/scala/SortingAlgorithm.scala
+++ src/main/scala/SortingAlgorithm.scala
@@ -4,7 +4,7 @@ object SortingAlgorithm  extends App{
   def selectionSort(list : Array[Int]): Array[Int] = {
     for (i <- 0 until (list.length -1)){
       var min = i
-       for(j <- (i+1) until list.length){
+      for(j <- (i+1) until list.length){
         if (list(j) < list(min))
           min = j
       }
@@ -13,6 +13,19 @@ object SortingAlgorithm  extends App{
         list(i) = list(min);
         list(min) = swap;
       }
+    }
+    return list
+  }
+  //Insertion Sort
+  def insertionSort(list: Array[Int]): Array[Int] = {
+    for(i <- 1 until list.length){
+      val temp = list(i)
+      var j = i-1
+      while(j > -1 && list(j) > temp){
+        list(j+1) = list(j)
+        j -= 1
+      }
+      list(j+1) = temp
     }
     return list
   }
--- src/test/scala/SortingAlgorithmSpec.scala
+++ src/test/scala/SortingAlgorithmSpec.scala
@@ -27,4 +27,34 @@ class SortingAlgorithmSpec extends FlatSpec with Matchers {
     val expected = Array(5, 10, 10, 10, 20, 20, 30, 30, 70, 90)
     actual  should  be (expected)
   }
+  "Insertion Sort of Empty Array ()" should "be ()" in {
+    val input = Array.empty[Int]
+    val actual = insertionSort(input)
+    val expected = Array.empty[Int]
+    actual  should  be (expected)
+  }
+  "Insertion Sort of an  un sorted Array (10, 30, 20, 5, 70, 90)" should "be (5, 10, 20, 30, 70, 90)" in {
+    val input = Array(10, 30, 20, 5, 70, 90)
+    val actual = insertionSort(input)
+    val expected = Array(5, 10, 20, 30, 70, 90)
+    actual  should  be (expected)
+  }
+  "Insertion Sort of sorted Array (5, 10, 20, 30, 70, 90)" should "be (5, 10, 20, 30, 70, 90)" in {
+    val input = Array(5, 10, 20, 30, 70, 90)
+    val actual = insertionSort(input)
+    val expected = Array(5, 10, 20, 30, 70, 90)
+    actual  should  be (expected)
+  }
+  "Insertion Sort of a Reverse sorted Array (90, 70, 30, 20, 10, 5)" should "be (5, 10, 20, 30, 70, 90)" in {
+    val input = Array(90, 70, 30, 20, 10, 5)
+    val actual = insertionSort(input)
+    val expected = Array(5, 10, 20, 30, 70, 90)
+    actual  should  be (expected)
+  }
+  "Insertion Sort of one element  Array (90)" should "be ( 90)" in {
+    val input = Array(90)
+    val actual = insertionSort(input)
+    val expected = Array(90)
+    actual  should  be (expected)
+  }
 }
